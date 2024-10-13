e8c813f185895bd87e8d32566f4f9eef040f1370
Javed-Malik <javedfbr39@gmail.com>
2024-10-13 07:46:10 -0700

    add some color styling

--- React/Accordion Component/accordion_component/src/App.js
+++ React/Accordion Component/accordion_component/src/App.js
@@ -28,7 +28,7 @@ function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} />
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
@@ -39,7 +39,7 @@ function AccordionItem({ num, title, text }) {
     setIsOpen((isOpen) => !isOpen);
   }
   return (
-    <div className="item" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
