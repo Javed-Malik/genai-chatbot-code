f94553c7163dd8873136abcb2f86f83b871f3ed1
Javed-Malik <javedfbr39@gmail.com>
2024-10-13 00:28:44 -0700

    final

---
+++ React/Travel List/travel_list/src/components/Status.js
@@ -0,0 +1,20 @@
+export default function Status({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start Adding some items to your packing list ⚡</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `📝You have ${numItems} items in your list, and you are already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
