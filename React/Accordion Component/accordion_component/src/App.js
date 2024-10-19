a7870532c66d25c68df3140466ffff7fce2d0c90
Javed-Malik <javedfbr39@gmail.com>
2024-10-19 00:51:09 -0700

    child props

--- React/Accordion Component/accordion_component/src/App.js
+++ React/Accordion Component/accordion_component/src/App.js
@@ -33,15 +33,16 @@ function Accordion({ data }) {
           curOpen={curOpen}
           onOpen={setCurOpen}
           title={el.title}
-          text={el.text}
           num={i}
           key={el.title}
-        />
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
-function AccordionItem({ num, title, text, curOpen, onOpen }) {
+function AccordionItem({ num, title, text, curOpen, onOpen, children }) {
   const isOpen = num === curOpen;
   function handleToggle() {
     onOpen(isOpen ? null : num);
@@ -52,7 +53,7 @@ function AccordionItem({ num, title, text, curOpen, onOpen }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text} </div>}
+      {isOpen && <div className="content-box">{children} </div>}
     </div>
   );
 }
