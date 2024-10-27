b7a227417edd4e77d36d018a552956dc15e6bdcf
Javed-Malik <javedfbr39@gmail.com>
2024-10-26 22:19:44 -0700

    star rating

--- React/usePopcorn/usepopcorn/src/index.js
+++ React/usePopcorn/usepopcorn/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size="23"
+      className="test"
+      messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={5} />
+    {/* <App /> */}
   </React.StrictMode>
 );
